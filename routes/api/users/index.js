5879bb1e822c16cb33f942fd3c66879b85f79195
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2021-03-04 21:01:00 +0200

    fead: adds possibility to logout users

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../../../controllers/users-controller');
+const guard = require('../../../helpers/guard');
 const validate = require('./validation');
 
 router.post('/signup', validate.createUser, usersController.create);
 router.post('/login', usersController.login);
-router.post('/logout', usersController.logout);
+router.post('/logout', guard, usersController.logout);
 
 module.exports = router;
